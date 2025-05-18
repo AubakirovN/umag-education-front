29c225023f9088df7216d876c091dec45a9d2fd1
kazba <kazba_anuarbekov@gmail.com>
2025-05-18 18:57:59 +0500

    fixes

--- src/features/Landing/components/ClientCourse/ClientCourseProgram.tsx
+++ src/features/Landing/components/ClientCourse/ClientCourseProgram.tsx
@@ -1,4 +1,4 @@
-import { Card, Flex, Text, Title } from "@mantine/core";
+import { Accordion, Card, Flex, Text, Title } from "@mantine/core";
 import styles from "./ClientCourse.module.css";
 import ReactPlayer from "react-player/youtube";
 
@@ -16,35 +16,67 @@ export const ClientCourseProgram = ({ course }: any) => {
           backgroundColor: "#f5f5f5",
         }}
       >
-        {course?.course_blocks?.sort((a:any,b:any) => a.number - b.number)?.map((item:any, key:any) => (
-          <Flex key={key}>
-            <Flex direction="column" key={key}>
-              <Flex gap={10}>
-                <span style={{ color: "#2DBE61" }}>Блок {item.number}</span>
-                <Text fw={500}>{item.title}</Text>
-              </Flex>
-              {item?.lessons?.map((el:any, index:any) => (
-                <Flex direction="column">
-                  <Text style={{ paddingLeft: '5px', borderLeft: "4px solid #2DBE61" }}>
-                    Урок {index + 1}. {el?.title}
-                  </Text>
-                  <Text dangerouslySetInnerHTML={{ __html: el?.description }} />
-                  {el?.video_url && (
-                    <div style={{ position: "relative", paddingTop: "56.25%" }}>
-                      <ReactPlayer
-                        url={el?.video_url}
-                        width="100%"
-                        height="100%"
-                        style={{ position: "absolute", top: 0, left: 0 }}
-                        controls
-                      />
-                    </div>
-                  )}
-                </Flex>
+        {course?.course_blocks?.length > 0 && (
+          <Accordion chevronPosition="right" variant='separated'>
+            {course?.course_blocks
+              ?.sort((a: any, b: any) => a.number - b.number)
+              ?.map((item: any) => (
+                <Accordion.Item value={item.id.toString()} key={item.id}>
+                  <Accordion.Control>
+                    <Flex w="100%">
+                      <Flex w="100%" direction="column">
+                        <Flex gap={10}>
+                          <span style={{ color: "#2DBE61" }}>
+                            Блок {item.number}
+                          </span>
+                          <Text fw={500}>{item.title}</Text>
+                        </Flex>
+                      </Flex>
+                    </Flex>
+                  </Accordion.Control>
+                  <Accordion.Panel>
+                    {item?.lessons?.map((el: any, index: any) => (
+                      <Flex key={index} direction="column">
+                        <Text
+                          style={{
+                            paddingLeft: "5px",
+                            borderLeft: "4px solid #2DBE61",
+                          }}
+                        >
+                          Урок {index + 1}. {el?.title}
+                        </Text>
+                        <Text
+                          dangerouslySetInnerHTML={{
+                            __html: el?.description,
+                          }}
+                        />
+                        {el?.video_url && (
+                          <div
+                            style={{
+                              position: "relative",
+                              paddingTop: "56.25%",
+                            }}
+                          >
+                            <ReactPlayer
+                              url={el?.video_url}
+                              width="100%"
+                              height="100%"
+                              style={{
+                                position: "absolute",
+                                top: 0,
+                                left: 0,
+                              }}
+                              controls
+                            />
+                          </div>
+                        )}
+                      </Flex>
+                    ))}
+                  </Accordion.Panel>
+                </Accordion.Item>
               ))}
-            </Flex>
-          </Flex>
-        ))}
+          </Accordion>
+        )}
       </Card>
     </Flex>
   );
